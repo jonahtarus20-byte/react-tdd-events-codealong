cb18842d50c6b68faf02340e37ed01e28ebfcb16
Your Name <youremail@example.com>
2025-10-18 17:31:22 +0300

    finished assignment

--- src/App.js
+++ src/App.js
@@ -1,7 +1,42 @@
+import React, { useState } from "react";
+
 function App() {
+  const [toppings, setToppings] = useState(["Cheese"]);
+  const [isPepperoniChecked, setIsPepperoniChecked] = useState(false);
+
+  function handleCheckboxChange() {
+    setIsPepperoniChecked(!isPepperoniChecked);
+
+    setToppings((prevToppings) => {
+      if (isPepperoniChecked) {
+        // If it was checked before, remove pepperoni
+        return prevToppings.filter((topping) => topping !== "Pepperoni");
+      } else {
+        // If it was unchecked before, add pepperoni
+        return [...prevToppings, "Pepperoni"];
+      }
+    });
+  }
+
   return (
-    <div></div>
-  )
+    <div>
+      <label>
+        <input
+          type="checkbox"
+          checked={isPepperoniChecked}
+          onChange={handleCheckboxChange}
+          aria-label="Add Pepperoni"
+        />
+        Add Pepperoni
+      </label>
+
+      <ul>
+        {toppings.map((topping) => (
+          <li key={topping}>{topping}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
